cc42a442ff459e6ffbbdebc5f95c4576fcf95a4e
Gokulust <gokul.rajashekharan@ust.com>
2023-10-19 01:33:31 +0530

    assignment added

---
+++ Assignments/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Assignments;
+
+Console.WriteLine("Enter the student name:");
+string? studentName=Console.ReadLine();
+Console.WriteLine("Enter the first Mark:");
+double markOne=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the second Mark:");
+double markTwo = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the third Mark:");
+double markThree = Convert.ToDouble(Console.ReadLine());
+
+Student student=new Student(studentName,markOne, markTwo, markThree);
+Console.WriteLine($"Total Mark of student {student.StudentName} And Average Mark is {student.CalculateAverage()} with grade {student.GetGrade()}");
---
+++ Assignments/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class Student
+    {
+        public string? StudentName;
+        double markOne, markeTwo, markThree;
+
+        public Student(string studentName, double markOne, double markeTwo, double markThree)
+        {
+            this.StudentName = studentName;
+            this.markOne = markOne;
+            this.markeTwo = markeTwo;
+            this.markThree = markThree;
+        }
+        public double CalculateAverage()
+        {
+            double average;
+            average = (markOne + markeTwo + markThree) / 3;
+            return average;
+        }
+        public double GetTotalMarks()
+        {
+            double totalMarks;
+            totalMarks = markOne + markeTwo + markThree;
+            return totalMarks;
+        }
+        public char GetGrade()
+        {
+            double average=CalculateAverage();
+            if(average >= 90)
+            {
+                return 'A';
+            }
+            else if(average >=80)
+            {
+                return 'B';
+            }
+            else if(average >=70)
+            {
+                return 'C';
+            }
+            else if(average >=60)
+            {
+                return 'D';
+            }
+            else if (average >=50)
+            {
+                return 'E';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+    }
+}
