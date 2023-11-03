24a929f0d050c5f794ff692c0dbceaf82b9af670
GokulUST <148053158+Gokulust@users.noreply.github.com>
2023-11-03 15:13:07 +0530

    Update Program.cs

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -269,6 +269,31 @@ TaskItem.UpdateTask(14);
 
 TaskItem.Display();
 
+ public string Name { get; set; }
+    public int Age { get; set; }
+    public List<FamilyMember> Children { get; set }
+
+    public FamilyMember(string name, int age)
+    {
+        Name = name;
+        Age = age;
+        Children = new List<FamilyMember>();
+    }
+
+    public void AddChild(FamilyMember child)
+    {
+        Children.Add(child);
+    }
+
+    public void DisplayFamily(int level = 0)
+    {
+        string indent = new string(' ', 2 * level);
+        Console.WriteLine($"{indent}{Name}, Age: {Age}");
+        foreach (var child in Children)
+        {
+            child.DisplayFamily(level + 1);
+        }
+    }
 
 
 
